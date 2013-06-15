d1dd993ca540fde5e87944ce7d10774afc65f07f
Sajjad Anwar <sajjadkm@gmail.com>
2013-06-15 18:54:29 +0530

    Produce a list of missing boundaries

--- centroid.py
+++ centroid.py
@@ -8,15 +8,15 @@ def configure():
         return settings
     return None
 
-queries = {'get_clusters':"select bcoord.id_bndry, ST_AsText(bcoord.coord),initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Cluster' and b.id=bcoord.id_bndry order by b.name",
+queries = {'get_clusters':"select bcoord.id_bndry, initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Cluster' and b.id=bcoord.id_bndry order by b.name",
               'get_schools':"select distinct s.id, s.name from tb_boundary b, tb_boundary b1, tb_boundary b2,tb_school s,tb_bhierarchy hier where b.id=b1.parent and b1.id=b2.parent and s.bid=b2.id and b.hid = hier.id and b.type=1 and b2.id=%s order by s.name",
               'get_clusters_centroid':"select b2.id,'Cluster', ST_AsText(ST_CENTROID(ST_COLLECT(ic.coord))) from vw_inst_coord ic, tb_boundary b2, tb_school s where b2.type=1 and s.bid=b2.id and ic.instid = s.id and b2.id=%s GROUP BY b2.id",
-              'get_blocks':"select bcoord.id_bndry,ST_AsText(bcoord.coord),initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Block' and b.id=bcoord.id_bndry order by b.name",
+              'get_blocks':"select bcoord.id_bndry, initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Block' and b.id=bcoord.id_bndry order by b.name",
               'get_block_clusters':"select distinct b2.id, b2.name from tb_boundary b, tb_boundary b1, tb_boundary b2,tb_bhierarchy hier where b.id=b1.parent and b1.id=b2.parent  and b.hid = hier.id and b.type=1 and b1.id=%s order by b2.name",
               'get_blocks_centroid':"select b1.id, 'Block', ST_AsText(ST_CENTROID(ST_COLLECT(ic.coord))) from vw_inst_coord ic, tb_boundary b1, tb_boundary b2, tb_school s where b1.type=1 and s.bid = b2.id and s.id =ic.instid and b1.id=b2.parent and b1.id=%s GROUP BY b1.id",
-              'get_circles':"select bcoord.id_bndry,ST_AsText(bcoord.coord),initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Circle' and b.id=bcoord.id_bndry order by b.name",
+              'get_circles':"select bcoord.id_bndry, initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Circle' and b.id=bcoord.id_bndry order by b.name",
               'get_circles_centroid':"select b2.id,'Circle',ST_AsText(ST_CENTROID(ST_COLLECT(ic.coord))) from vw_inst_coord ic, tb_boundary b2, tb_school s where b2.type=2 and s.bid=b2.id and ic.instid = s.id and b2.id=%s GROUP BY b2.id",
-              'get_projects':"select bcoord.id_bndry,ST_AsText(bcoord.coord),initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Project' and b.id=bcoord.id_bndry order by b.name",
+              'get_projects':"select bcoord.id_bndry, initcap(b.name) from vw_boundary_coord bcoord, tb_boundary b where bcoord.type='Project' and b.id=bcoord.id_bndry order by b.name",
               'get_projects_centroid':"select b1.id, 'Project', ST_AsText(ST_CENTROID(ST_COLLECT(ic.coord))) from vw_inst_coord ic, tb_boundary b1, tb_boundary b2, tb_school s where b1.type=2 and s.bid = b2.id and s.id =ic.instid and b1.id=b2.parent and b1.id=%s GROUP BY b1.id"
               }
 
@@ -34,6 +34,7 @@ def main():
     cursor = conn.cursor()
 
     operations = open('operations.sql','w')
+    missed = open('missing.list', 'w')
 
     boundaries = ['clusters', 'blocks', 'circles', 'projects']
     counts = {'clusters':{'total':0, 'missed':0}, 'blocks':{'total':0, 'missed':0},
@@ -46,15 +47,19 @@ def main():
         found = 0
 
         operations.write("#"+boundary+'\n')
+        missed.write("#"+boundary+'\n')
         for d in data:
-            id = list(d)[0]
-            cursor.execute(queries['get_'+boundary+'_centroid'],(id,))
-            centroids = cursor.fetchall()
-            if centroids:
-                found = found+1
-                centroid = list(centroids[0])
-                query = "INSERT INTO boundary_coord VALUES ("+str(centroid[0])+", '"+centroid[1]+"', ST_GeomFromText('"+centroid[2]+"',"+"4326))"
-                operations.write(query+';\n')
+          id = list(d)[0]
+          cursor.execute(queries['get_'+boundary+'_centroid'],(id,))
+          centroids = cursor.fetchall()
+          if centroids:
+              found = found+1
+              centroid = list(centroids[0])
+              query = "INSERT INTO boundary_coord VALUES ("+str(centroid[0])+", '"+centroid[1]+"', ST_GeomFromText('"+centroid[2]+"',"+"4326))"
+              operations.write(query+';\n')
+          else:
+            missed.write(str(list(d)[0])+", "+list(d)[1]+"\n")
+
         counts[boundary]['missed']=(data.__len__())-found
     print counts
 if __name__ == '__main__':
